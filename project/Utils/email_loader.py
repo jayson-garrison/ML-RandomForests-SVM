49d1a5398414f986c0010de8e4237c661b475d51
Jayson C. Garrison <jcg5493@utulsa.edu>
2022-02-11 15:14:18 -0600

    added five_fold

--- project/Utils/email_loader.py
+++ project/Utils/email_loader.py
@@ -8,12 +8,12 @@ def load_email_data():
     data = pd.read_csv('./project/Datasets/spam_ham/emails.csv')
     data = data.to_numpy()
     np.random.shuffle(data)
-    X = data[:, 1:5] # fix the indeces
-    Y = data[:, -1]
+    X = data[1:, 1:-1] # fix the indeces
+    Y = data[1:, -1]
     # TODO build Sample objects with label and a vector for X
     # Example:
     samples = list()
-    for i in range(len(data)):
+    for i in range(len(Y)):
         sample = Sample(label=Y[i], X=X[i])
         samples.append(sample)
     return samples
---
+++ project/Utils/five_fold.py
@@ -0,0 +1,25 @@
+""""
+Author: Jayson C. Garrison
+Dates: 02/11/2022
+Course: CS-5333 (Machine Learning)
+Description: Function that partitions data according to five fold cross validation
+GitHub: https://github.com/jayson-garrison/ML-Naive-Bayes
+"""
+# partition a data set into a list of 5 tuples for training and testing
+# five fold data partition
+def five_fold(data_set):
+    partition_index = int( len(data_set) / 5 )
+    print('pdex: ', partition_index) 
+    s = 0
+    fold = []
+    for i in range(5): #0-4
+        tr = data_set.copy()
+        n = s + partition_index # was -1
+        te = tr[s:n]
+        del tr[s:s + partition_index]
+
+        fold.append( (tr,te) )
+
+        s += partition_index
+
+    return fold
