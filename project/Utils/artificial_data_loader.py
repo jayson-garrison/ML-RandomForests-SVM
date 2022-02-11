1515d6f404c0c95c2bb5f3ceb1fbe51e70a0bf0b
Jayson C. Garrison <jcg5493@utulsa.edu>
2022-02-11 15:43:59 -0600

    added artificial_data_set loader

--- project/Utils/artificial_data_loader.py
+++ project/Utils/artificial_data_loader.py
@@ -3,12 +3,17 @@ import numpy as np
 from Utils.Sample import Sample
 from Utils.Attribute import Attribute
 
-def load_image_data():
-    data = pd.read_csv('./project/Datasets/mnist/train.csv') 
+def load_artificial_data(path):
+    '''
+    load the artificial data set given a path
+    @param path
+    @returns samples
+    '''
+    data = pd.read_csv(path)
     data = data.to_numpy()
     np.random.shuffle(data)
-    X = data[1:, 1:]
-    Y = data[1:, 0]
+    X = data[1:, 1:-1] # i believe this is correct
+    Y = data[1:, -1]
     # TODO build Sample objects with label and a vector for X
     # Example:
     samples = list()
