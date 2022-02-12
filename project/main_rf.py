76a14888bcbb2198549a25de8a775d9e60f7d29a
Andrew DeVoss <apd0340@utulsa.edu>
2022-02-12 11:36:06 -0600

    Started driver

--- project/main_rf.py
+++ project/main_rf.py
@@ -1,9 +1,19 @@
-from Utils.email_loader import load_email_data
 from RandomForest.RandomForestModel import *
+from Utils.email_loader import load_email_data
+from project.Utils.five_fold import five_fold
 
 if __name__ == "__main__":
     dataset, attributes = load_email_data()
+    partition = five_fold(dataset)
+
+    # for idx in range(len(partition)):
+    #     training = list()
+    #     testing = partition[i]
     rf = RandomForestModel(maxTreeDepth=3)
-    initial_parents = set()
-    tree = rf.learn_decision_tree(dataset, attributes, initial_parents)
-    tree.pretty_print_tree()
+    rf.fit(partition[0], attributes, 5)
+    num, den = 0, 0
+    for sample in partition[1]:
+        prediction = rf.call(sample)
+        if prediction == sample.getLabel(): num += 1
+        den += 1
+    print(f'Validation accuracy: {num/den}')
