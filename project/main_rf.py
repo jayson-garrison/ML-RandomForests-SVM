8424d2949609af2c05e979727a6a6c3309f83a7f
Jayson C. Garrison <jcg5493@utulsa.edu>
2022-02-15 07:13:12 -0600

    added param_list_1; should be ready for testing

--- project/main_rf.py
+++ project/main_rf.py
@@ -10,7 +10,7 @@ from Utils.visualize_log import *
 
 if __name__ == "__main__":
     run_analysis = True
-    params_list = [
+    params_list_0 = [
         (1, 100, entropy, 'entropy', 'spiral'),
         (1, 100, gini, 'gini', 'spiral'),
         (1, 100, misclassification, 'misclassification', 'spiral'),
@@ -30,8 +30,28 @@ if __name__ == "__main__":
         (3, 100, gini, 'gini', 'image'),
         (3, 100, misclassification, 'misclassification', 'image'),
     ]
+    params_list_1 = [
+        (1, 100, entropy, 'entropy', 'mail'),
+        (1, 100, gini, 'gini', 'mail'),
+        (1, 100, misclassification, 'misclassification', 'mail'),
+        (2, 100, entropy, 'entropy', 'mail'),
+        (2, 100, gini, 'gini', 'mail'),
+        (2, 100, misclassification, 'misclassification', 'mail'),
+        (3, 100, entropy, 'entropy', 'mail'),
+        (3, 100, gini, 'gini', 'mail'),
+        (3, 100, misclassification, 'misclassification', 'mail'),
+        (1, 100, entropy, 'entropy', 'blob'),
+        (1, 100, gini, 'gini', 'blob'),
+        (1, 100, misclassification, 'misclassification', 'blob'),
+        (2, 100, entropy, 'entropy', 'blob'),
+        (2, 100, gini, 'gini', 'blob'),
+        (2, 100, misclassification, 'misclassification', 'blob'),
+        (3, 100, entropy, 'entropy', 'blob'),
+        (3, 100, gini, 'gini', 'blob'),
+        (3, 100, misclassification, 'misclassification', 'blob'),
+    ]
     
-    for params in params_list:
+    for params in params_list_0: # 0 or 1
         hyper_parameters = {
             'mdt': params[0], # Max depth of the tree
             'nt': 100, # Number of trees in the forest
