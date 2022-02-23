Fixed overflow

project/SupportVectorMachine/SVM.py
@@ -1,14 +1,11 @@
+from cmath import inf
 from Utils.Model import Model
 import numpy as np
 from numpy.linalg import norm
 
 class SVM(Model):
-    def __init__(self, X, Y, C, tol, max_passes, k='inner_product'):
+    def __init__(self, C, tol, max_passes, k='inner_product'):
         super().__init__()
-        self.X = X
-        self.Y = Y
-        self.alpha = np.zeros(Y.size)
-        self.b = 0
         self.C = C
         self.tol = tol
         self.max_passes = max_passes
@@ -24,14 +21,20 @@ class SVM(Model):
         }
         return info
 
-    def call(self, sample):
-        guess = self.f(sample.getX())
+    def call(self, x):
+        guess = self.f(x)
         if guess < 0: return -1
         return 1
 
-    def fit(self):
+    def fit(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.alpha = np.zeros(Y.size)
+        self.b = 0
+
         passes = 0
         log_vals = False
+        prevent_overflow = True
 
         while(passes < self.max_passes):
             num_changed_alphas = 0
@@ -56,14 +59,26 @@ class SVM(Model):
 
                     # computing L and H using old alphas
                     if (self.Y[i] != self.Y[j]):
-                        L = max( [0, a_j_old - a_i_old] )
-                        H = min( [self.C, self.C + a_j_old - a_i_old] )
+                        # L = max( [0, a_j_old - a_i_old] )
+                        # H = min( [self.C, self.C + a_j_old - a_i_old] )
+                        L = 0 if 0 > (a_j_old - a_i_old) else (a_j_old - a_i_old)
+                        if L > self.C: L = self.C
+                        H = self.C if self.C < self.C + a_j_old - a_i_old else self.C + a_j_old - a_i_old
+                        if H < 0: H = 0
                     else:
-                        L = max( [0, a_i_old + a_j_old - self.C] )
-                        H = min( [self.C, a_i_old + a_j_old] )
+                        # L = max( [0, a_i_old + a_j_old - self.C] )
+                        # H = min( [self.C, a_i_old + a_j_old] )
+                        L = 0 if 0 > (a_i_old + a_j_old - self.C) else (a_i_old + a_j_old - self.C)
+                        if L > self.C: L = self.C
+                        H = self.C if self.C < (a_i_old + a_j_old) else (a_i_old + a_j_old)
+                        if H < 0: H = 0
                     if (L == H):
                         if log_vals: print()
                         continue
+
+                    if L>self.C or H<0:
+                        print(f'\n\na_i_old:{a_i_old}\na_j_old:{a_j_old}\nL:{L}\nH:{H}')
+                        exit()
 
                     # get longN
                     long_n = self.longN(i, j)
@@ -78,8 +93,12 @@ class SVM(Model):
                     # clip a_j
                     if (a_j > H):
                         a_j = H
+                        if log_vals: print(f" a_j was CLIPPED TO H={H} ", end='')
                     elif ( a_j < L):
                         a_j = L
+                        if log_vals: print(f" a_j was CLIPPED TO L={L} ", end='')
+                    else:
+                        if log_vals: print(" a_j was UNCLIPPED", end='')
                     # otherwise a_j is correctly computed
 
                     if (abs(a_j - a_j_old) < 10**-5):
@@ -107,6 +126,15 @@ class SVM(Model):
                     if log_vals: print(f'b_1={round(b_1, 4)} ', end='')
                     if log_vals: print(f'b_2={round(b_2, 4)} ', end='')
                     if log_vals: print(f'b={round(self.b, 4)}')
+                    if log_vals or prevent_overflow: 
+                        if (abs(E_i)  >= 100000 or \
+                            abs(E_j)  >= 100000 or \
+                            abs(a_i)  >= 100000 or \
+                            abs(a_j)  >= 100000 or \
+                            abs(long_n) >= 100000):
+                            print(f'OVERFLOW:')
+                            print(f'\n\na_i_old:{a_i_old}\na_j_old:{a_j_old}\nL:{L}\nH:{H}')
+                            exit()
                     
                     self.alpha[i] = a_i
                     self.alpha[j] = a_j
@@ -132,6 +160,10 @@ class SVM(Model):
         long_n = 2 * self.kernel(self.X[i], self.X[j]) - self.kernel(self.X[i], self.X[i]) -\
                                                                     self.kernel(self.X[j], self.X[j])
         return long_n
+
+    def reset(self):
+        self.alpha = np.zeros(self.Y.size)
+        self.b = 0
 
     def kernel(self, x1, x2):
         """

project/Utils/email_loader.py
@@ -1,5 +1,7 @@
+from re import L
 import pandas as pd
 import numpy as np
+from sklearn.utils import indices_to_mask
 from Utils.Sample import Sample
 from Utils.Attribute import Attribute
 from Utils.PCA import *
@@ -9,11 +11,25 @@ from Utils.helper_functions import select_attributes
 def load_email_data(using_svm=False):
     data = pd.read_csv('./project/Datasets/spam_ham/pca_emails.csv')
     data = data.to_numpy()
-    X = data[1:, 1:-1]
+    X = data[1:, 2:]
     #X = pca(X, 4)
-    Y = data[1:, -1]
+    Y = data[1:, 1]
     #pca_emails = pd.DataFrame(np.column_stack((Y,X))) 
     #pca_emails.to_csv('pca_emails.csv')
+
+    #========================================================
+    # Uncomment this routine to keep only a subset of the samples
+    num_samples = 500
+    indices = set()
+    while(len(indices) < num_samples):
+        indices.add(np.random.randint(len(Y)))
+    indices = list(indices)
+    indices.sort()
+    X = np.take(X, indices, axis=0)
+    X = norm_0_1(X)
+    Y = np.take(Y, indices, axis=0)
+    #========================================================
+
     
     samples = list()
     attr_dict = dict()
@@ -31,7 +47,7 @@ def load_email_data(using_svm=False):
     for key in attr_dict:
         attr_values = list(attr_dict[key])
         attributes.append(Attribute(name=key, values=select_attributes(attr_values, 
-                                                                        12 # The number of attibute values to consider as possible split points.
+                                                                        min(12, len(attr_values)) # The number of attibute values to consider as possible split points.
                                                                         )
                                     )
                         )

project/Utils/five_fold.py
@@ -12,10 +12,8 @@ import time
 # five fold data partition
 def five_fold(data_set):
     """[summary]
-
     Args:
         data_set (List of Sample objects): The Samples to be partitioned
-
     Returns:
         fold: where fold is list of len n in n-fold of (train,test) where train and test are lists of Samples
     """
@@ -33,6 +31,7 @@ def five_fold(data_set):
         s += partition_index
 
     return fold
+
 
 
 def make_output_strings_rf(dataset, model, attributes):
@@ -89,28 +88,35 @@ def make_output_strings_rf(dataset, model, attributes):
     return output
 
 
-# NOTE: please ignore the fact that this is a virtual copy of make_output_strings_rf()
-def make_output_strings_svm(dataset, model):
-    fold = five_fold(dataset)
+def five_fold_svm(matrix):
+    matrix = np.split(matrix, 5)
+    return matrix
+
+
+# NOTE: necessarily different from make_output_strings_rf
+def make_output_strings_svm(X, Y, model):
+    fold = five_fold_svm(np.column_stack((Y, X)))
     output = ''
     fold_ctr = 0
     training_accuracies = [0 for _ in range(5)]
     testing_accracies = [0 for _ in range(5)]
 
-    for partition in fold:
+    for yx in fold:
+        y = yx[:, 0]
+        x = yx[:, 1:]
         start = time.time()
         fold_ctr += 1
         print(f'Fitting model [{fold_ctr}/5]', end=' ... ')
-        model.fit()
+        model.fit(x, y)
         print(f'Elapsed Time: {time.time()-start}')
         start = time.time()
         print(f'Validating model [{fold_ctr}/5]', end=' ... ')
         # First compute the training accuracies
         correct = 0
         total = 0
-        for sample in partition[0]:
-            prediction = model.call(sample) 
-            label = sample.getLabel()
+        for idx in range(len(y)):
+            prediction = model.call(x[idx]) 
+            label = y[idx]
             if label == prediction:
                 correct += 1
             total += 1
@@ -120,9 +126,9 @@ def make_output_strings_svm(dataset, model):
         # Now compute the testing accuracies
         correct = 0
         total = 0
-        for sample in partition[1]:
-            prediction = model.call(sample)
-            label = sample.getLabel()
+        for idx in range(len(y)):
+            prediction = model.call(x[idx]) 
+            label = y[idx]
             if label == prediction:
                 correct += 1
             total += 1